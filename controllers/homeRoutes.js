f2559e4c21759a476de9fa2d03f3368581ac2b76
MChambersIV <chambersm025@gmail.com>
2022-02-08 21:48:56 -0800

    added a fragment of get route for homeroutes.js, added blank .js files representing user and project table models in models directory

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { Projects } = require('../models');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const 
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
